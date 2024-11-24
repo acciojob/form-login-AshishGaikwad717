49478fa094f81df15064799966f462b9247bc5b7
Ashish Gaikwad <36022739+AshishGaikwad717@users.noreply.github.com>
2024-11-24 20:43:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormvalue() {
     //Write your code here
-	let fname = document.getElementById("fname");
-	let lname = document.getElementById("lname");
+	let fname = document.getElementById('fname);
+	let lname = document.getElementById('lname');
 	window.alert(fname+" "+lname);
 }
