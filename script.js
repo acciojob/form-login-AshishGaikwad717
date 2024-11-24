3863dc9c831db5e303e7233a66758eebc8d5c26c
Ashish Gaikwad <36022739+AshishGaikwad717@users.noreply.github.com>
2024-11-24 20:55:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormvalue() {
     //Write your code here
-	const form = document.querySelector("form1");
+	const form = document.querySelector("form");
 	const firstName = form.elements["fname"].value;
 	const lastName = form.elements["lname"].value;
 	alert(`${firstName} ${lastName}`);
